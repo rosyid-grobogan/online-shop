bd14d92aaba05f81ee658be2b082223c97496884
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-04 17:38:56 +0700

    Login and Auth

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adminUser = [
+            'name' => 'Admin',
+            'email' => 'admin@abc.com',
+            'password' => bcrypt('admin'),
+        ];
+
+        if ( !User::where('email', $adminUser['email'])->exists() ) {
+            User::create($adminUser);
+        }
+    }
+}
